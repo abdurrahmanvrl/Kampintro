645fde98d9b3a545ff4c62e37d18fa9ba0a6c132
abdurrahmanvarol <abdurrahmanvarol@ABDURRAHMAN98D4>
2022-07-17 17:17:33 +0300

    Add project files.

---
+++ Matematik/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using static Matematik.DortIslem;
+
+
+
+namespace Matematik // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DortIslem dortIslem = new DortIslem();
+            dortIslem.Topla(5, 6);
+            dortIslem.Topla(6, 9);
+        }
+    }
+}
